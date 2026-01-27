313bef501a76cba089c2e7e379dafcbdeabe470b
mikewhitetattoo <mikewhitetattoo@gmail.com>
2026-01-27 10:38:39 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -114,16 +114,3 @@ document.addEventListener('click', (e) => {
     navLinks.classList.remove('open');
   }
 });
-
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('open');
-});
-
-document.querySelectorAll('.nav-links a').forEach(link => {
-  link.addEventListener('click', () => {
-    navLinks.classList.remove('open');
-  });
-});
